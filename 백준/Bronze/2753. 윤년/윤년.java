7c0dc52f5c5bd07078126438cfaed8c6e6349d54
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-19 16:57:49 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        int isLeap = 0;
+        if(year%4 == 0){
+            if(year%100 == 0){
+                if(year%400 == 0){
+                    isLeap = 1;
+                }
+            }else{
+                isLeap = 1;
+            }
+        }
+        System.out.println(isLeap);
+    }
+}
