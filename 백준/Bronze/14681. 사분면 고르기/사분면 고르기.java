c863f6e691c8c05ecda5f128c4a54307b89ee816
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-19 17:39:25 +0900

    [Bronze V] Title: 사분면 고르기, Time: 212 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int quadrant = 0;
+        if(num1 > 0){
+           quadrant =  (num2 > 0) ? 1 : 4; 
+        }else{
+            quadrant = (num2 > 0) ? 2 : 3;
+        }
+        System.out.println(quadrant);
+    }
+}
