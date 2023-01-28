7e5d330117b642fec3493be9d458dad02216d64a
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-28 20:51:49 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        if((n&1)==0){
+            sum = (1+n)*(n/2);
+        }else{
+            sum = (1+n)*(n/2)+((n/2)+1);
+        }
+        System.out.println(sum);
+    }
+}
