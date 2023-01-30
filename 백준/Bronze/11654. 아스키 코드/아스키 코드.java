4de9821ee1006810c169ba3a46cd4c2158bb4a8c
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-30 23:35:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char n = sc.next().charAt(0);
+        int a = n;
+        System.out.println(a);
+    }
+}
