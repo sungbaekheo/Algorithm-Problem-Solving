58ca974dfff08daf75dab0da01791d1ec489e847
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-30 16:04:21 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 208 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numList = new int[31];
+
+        for(int i=0; i<28; i++){
+            numList[sc.nextInt()]++;
+        }
+
+        for(int i=1; i<=30; i++){
+            if(numList[i] == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
