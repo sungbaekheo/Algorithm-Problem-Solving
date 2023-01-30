32bd784a4db42fc8e3692912ce0cf24ff0f897da
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-30 22:41:50 +0900

    [Silver V] Title: 셀프 넘버, Time: 212 ms, Memory: 20536 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numList = new int[10001];
+        for(int i=1; i<numList.length; i++){
+            numList[selfNum(i)]++;
+        }
+        for(int i=1; i<numList.length; i++){
+            if(numList[i] == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    static int selfNum(int a){
+        int sum = 0;
+        char[] as = String.valueOf(a).toCharArray();
+        for(char c:as){
+            sum+=Integer.parseInt(String.valueOf(c));
+        }
+        if(a+sum > 10000){
+            return 0;
+        }
+        return a+sum;
+    }
+}
