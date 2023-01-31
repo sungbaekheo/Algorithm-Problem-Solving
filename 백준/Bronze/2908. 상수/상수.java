482ee986779f6023011e6d09ecf7e55ba44eced6
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-31 17:10:02 +0900

    [Bronze II] Title: 상수, Time: 236 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String temp = String.valueOf(a.charAt(2)) + String.valueOf(a.charAt(1)) + String.valueOf(a.charAt(0));
+        a = temp;
+        temp = String.valueOf(b.charAt(2)) + String.valueOf(b.charAt(1)) + String.valueOf(b.charAt(0));
+        b = temp;
+        System.out.println((Integer.parseInt(a) > Integer.parseInt(b))? Integer.parseInt(a) : Integer.parseInt(b));
+    }
+}
