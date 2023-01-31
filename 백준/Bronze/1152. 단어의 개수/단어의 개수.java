b086d143487e93f18918ce09e542eaa45f45318b
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-01-31 16:55:42 +0900

    [Bronze II] Title: 단어의 개수, Time: 488 ms, Memory: 30288 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+        int count = 1;
+        if(sentence.charAt(0) == ' '){
+            count--;
+        }
+        if(sentence.charAt(sentence.length()-1) == ' '){
+            count--;
+        }
+        for(int i=0; i<sentence.length(); i++){
+            if(sentence.charAt(i) == ' '){
+                count++;
+            };
+        }
+        System.out.println(count);
+    }
+}
