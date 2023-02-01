0c6027d7292295ae0f5427533f563b23a6ed5dc5
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-02 00:53:51 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 228 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+
+        for(int t=0; t<testCase; t++){
+            int fNum = sc.nextInt();
+            int rNum = sc.nextInt();
+            
+            int[] nResidents = new int[rNum+1];
+            for(int i=0; i<=rNum; i++){
+                nResidents[i] = i;
+            }
+
+            for(int i=0; i<fNum; i++){
+                int[] sumArray = new int[rNum+1];
+                for(int j=1; j<=rNum; j++){
+                    sumArray[j] = sumArray[j-1]+nResidents[j];
+                }
+                nResidents = sumArray;
+            }
+            System.out.println(nResidents[rNum]);
+        }
+    }
+}
