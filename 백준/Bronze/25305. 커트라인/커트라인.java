d565a2666a53b9b876c81dbc3eb399c501f47de7
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-04 01:45:19 +0900

    [Bronze II] Title: 커트라인, Time: 304 ms, Memory: 20904 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] scoreList = new int[n];
+
+        for(int i=0; i<n; i++){
+            scoreList[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<k; i++){
+            int max = scoreList[i];
+            int idx = 0;
+            for(int j=i+1; j<n; j++){
+                if(scoreList[j]>max){
+                    max = scoreList[j];
+                    idx = j;
+                }
+            }
+            scoreList[idx] = scoreList[i];
+            scoreList[i] = max;
+        }
+
+        System.out.println(scoreList[k-1]);
+
+
+    }
+}
