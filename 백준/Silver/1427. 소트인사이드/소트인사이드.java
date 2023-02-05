8d66b70c43b6b0093790989c1063f833c47eb879
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-05 09:58:39 +0900

    [Silver V] Title: 소트인사이드, Time: 196 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] number = sc.next().toCharArray();
+        int[] numbers = new int[10];
+        for(char c: number){
+            numbers[Character.getNumericValue(c)]++;
+        }
+
+        for(int i=9; i>=0; i--){
+            while(numbers[i] !=0){
+                System.out.print(i);
+                numbers[i]--;
+            }
+        }
+    }
+}
