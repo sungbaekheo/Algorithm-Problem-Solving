83f4898463396068abfcd8f746a8320a645384d0
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-08 23:59:40 +0900

    [Bronze II] Title: 블랙잭, Time: 216 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nCard = sc.nextInt();
+        int limit = sc.nextInt();
+        int[] cardList = new int[nCard];
+
+        for(int i=0; i<nCard; i++){
+            cardList[i] = sc.nextInt();
+        }
+
+        int maxPoint = 0;
+        for(int i=0; i<nCard; i++){
+            for(int j=i+1; j<nCard; j++){
+                for(int k=j+1; k<nCard; k++){
+                    int currentScore = cardList[i] + cardList[j] + cardList[k];
+                    if(currentScore > maxPoint && currentScore <= limit){
+                        maxPoint = currentScore;
+                    }
+                }
+            }
+        }
+        System.out.println(maxPoint);
+    }
+}
