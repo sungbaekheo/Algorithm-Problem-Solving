1af30ec1c8ee56c002df0afe1dcfd9afdb79abba
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-09 10:17:03 +0900

    [Silver I] Title: 직사각형, Time: 144 ms, Memory: 14592 KB -BaekjoonHub

---
+++ 백준/Silver/2527. 직사각형/직사각형.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        for(int t=0; t<4; t++){
+            int[] squares = new int[8];
+            st = new StringTokenizer(br.readLine());
+            char type = '0';
+            for(int i=0; i<8; i++){
+                squares[i] = Integer.parseInt(st.nextToken());
+            }
+            if((squares[2] < squares[4] || squares[3] < squares[5]) ||
+            (squares[6]<squares[0] || squares[1] > squares[7])) {
+                type = 'd';
+            } else if(squares[0] == squares[6] && squares[3] == squares[5] ||
+            squares[0] == squares[6] && squares[1] == squares[7] ||
+            squares[2] == squares[4] && squares[3] == squares[5] ||
+            squares[2] == squares[4] && squares[1] == squares[7]){
+                type = 'c';
+            } else if(squares[2] == squares[4]||squares[0] == squares[6]||squares[3] == squares[5]||squares[1] == squares[7]) {
+                type = 'b';
+            } else {
+                type = 'a';
+            }
+            sb.append(type+"\n");
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
