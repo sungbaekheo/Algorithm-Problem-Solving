4dd2c9d53caf57b54b743bac5c24f8081e6114e2
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-09 13:58:52 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 216 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mushPoint = 0;
+        int minAbs = 100;
+        int answer = 0;
+        for(int i=0; i<10; i++){
+            mushPoint+=sc.nextInt();
+            if(minAbs >= Math.abs(100-mushPoint)){
+                minAbs = Math.abs(100-mushPoint);
+                answer = mushPoint;
+            }
+        }
+        System.out.println(answer);
+    }
+}
