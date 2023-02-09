c7be363a30643743c8a7cc96756be3fc3b4cbdab
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-09 17:01:06 +0900

    [Silver V] Title: 수 이어가기, Time: 336 ms, Memory: 22768 KB -BaekjoonHub

---
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int maxCount = 0;
+        String answer = "";
+        for(int i=num; i>num/2; i--){
+            int initialNum = num;
+            int numberChain = i;
+            int count = 0;
+            String numList = "";
+            while(initialNum >= 0){
+                numList += initialNum+" ";
+                int temp = initialNum-numberChain;
+                initialNum = numberChain;
+                numberChain = temp;
+                count++;
+            }
+            if(count>maxCount){
+                maxCount = count;
+                answer = numList;
+            }
+        }
+        System.out.println(maxCount);
+        System.out.println(answer);
+    }
+}
