33a9de6b64201d140f7f22656898e1bfd3429915
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-14 21:37:22 +0900

    [Gold IV] Title: 좋다, Time: 380 ms, Memory: 22260 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int len = sc.nextInt();
+        int[] numList = new int[len];
+        for(int i=0; i<len; i++){
+            numList[i] = sc.nextInt();
+        }
+        Arrays.sort(numList);
+        int count = 0;
+        for(int i=0; i<len; i++){
+            int f = 0;
+            int p = len-1;
+            while(f < p){
+                if(numList[i] == numList[f]+numList[p]){
+                    if(f != i && p != i){
+                        count++;
+                        break;
+                    } else if(f == i){
+                        f++;
+                    } else if(p == i){
+                        p--;
+                    }
+                } else if(numList[f]+numList[p]>numList[i]){
+                    p--;
+                } else{
+                    f++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
