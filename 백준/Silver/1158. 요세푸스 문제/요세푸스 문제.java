a410967132aacad719fdd2db69fddde3d64fbf82
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-02-27 22:27:16 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 676 ms, Memory: 297968 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+
+        int nPeople = sc.nextInt();
+        int interval = sc.nextInt();
+
+        for(int i=1; i<=nPeople; i++){
+            q.offer(i);
+        }
+
+        sb.append("<");
+        while(!q.isEmpty()){
+            for(int i=0; i<interval-1; i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll());
+            if(q.size() != 0){
+                sb.append(", ");
+            } else {
+                sb.append(">");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
