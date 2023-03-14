b68bb48f5f89a00e7baf7e3517e2cb0d1fb809b8
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-03-14 23:57:59 +0900

    [Bronze IV] Title: 상근날드, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int burger = 2000;
+        int coke = 2000;
+
+        for(int i=0; i<3; i++){
+            burger = Math.min(burger, sc.nextInt());
+        }
+        for(int i=0; i<2; i++){
+            coke = Math.min(coke, sc.nextInt());
+        }
+
+        System.out.println(burger+coke-50);
+    }
+}
