b06d01afb6bd29586d0c6593a64b4742214db511
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-03-14 23:57:28 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 224 ms, Memory: 18252 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int L = sc.nextInt();
+        int N = sc.nextInt();
+
+        int ap = L*N;
+
+        int[] people = new int[5];
+        for(int i=0; i<5; i++){
+            people[i] = sc.nextInt() - ap;
+            System.out.print(people[i]+" ");
+        }
+    }
+}
