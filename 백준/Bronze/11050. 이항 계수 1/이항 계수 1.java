9cc5e3db0f371f612aeabd06079a323995eeed52
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-03-19 01:34:06 +0900

    [Bronze I] Title: 이항 계수 1, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int answer = 1;
+        for(int i=n; i>=1; i--){
+            answer*=i;
+        }
+        for(int i=n-k; i>=1; i--){
+            answer/=i;
+        }
+        for(int i=k; i>=1; i--){
+            answer/=i;
+        }
+
+        System.out.println(answer);
+    }
+}
