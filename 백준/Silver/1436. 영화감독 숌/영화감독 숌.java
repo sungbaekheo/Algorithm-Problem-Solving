cf6d2d036058d3530076425076262fb0b4fd209d
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-03-22 16:20:48 +0900

    [Silver V] Title: 영화감독 숌, Time: 380 ms, Memory: 89420 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int goal = sc.nextInt();
+    int count = 0;
+    int current = 666;
+    while(count != goal){
+      String curr = String.valueOf(current);
+      if(curr.contains("666")){
+        count++;
+      }
+      current++;
+    }
+    System.out.println(current-1);
+  }
+}
