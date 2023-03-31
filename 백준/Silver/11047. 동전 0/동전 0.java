15a36c944f7d766e6777f71c3d213e47249f6bc3
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-03-31 16:31:09 +0900

    [Silver IV] Title: 동전 0, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] coins = new int[N];
+    int K = sc.nextInt();
+    for(int i=0; i<N; i++){
+      coins[i] = sc.nextInt();
+    }
+
+    int nCoin = 0;
+
+    for(int i=coins.length-1; i>=0; i--){
+      nCoin+=K/coins[i];
+      K%=coins[i];
+      if(K == 0){
+        break;
+      }
+    }
+    System.out.println(nCoin);
+  }
+}
