125f720bccc3dfefe4174a0aaf30b9bff0f29dd5
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-04-06 08:21:41 +0900

    [Silver IV] Title: 주몽, Time: 460 ms, Memory: 34792 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int con = sc.nextInt();
+        int[] materials = new int[n];
+        int answer = 0;
+
+        for(int i=0; i<n; i++){
+            materials[i] = sc.nextInt();
+        }
+
+        Arrays.sort(materials);
+
+        int front = 0;
+        int rear = n-1;
+
+        while(front < rear){
+            int curr = materials[front] + materials[rear];
+            
+            if(curr == con){
+                answer++;
+                front++;
+                rear--;
+            } else if(curr < con){
+                front++;
+            } else {
+                rear--;
+            }
+        }
+
+        System.out.println(answer);
+        
+    }
+}
