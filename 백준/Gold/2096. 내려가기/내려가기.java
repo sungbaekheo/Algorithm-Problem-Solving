1c2498cc311e2104c5e56f1c3cc676b9154b2ec2
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-04-10 23:14:35 +0900

    [Gold V] Title: 내려가기, Time: 384 ms, Memory: 52884 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,15 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		int[][][] ladder = new int[N+1][3][2];
 		
 		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<3; j++) {
-				int input = sc.nextInt();
+				int input = Integer.parseInt(st.nextToken());
 				int min = ladder[i-1][j][0];
 				int max = ladder[i-1][j][1];
 
