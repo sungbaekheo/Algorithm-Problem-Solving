4df560af8babf45ed044e41b71dadf6463bf8a27
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-04-10 17:32:22 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
 		int[][] dp = new int[N+1][3];
 		
 		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<3; j++) {
-				int input = sc.nextInt();
+				int input = Integer.parseInt(st.nextToken());
 				dp[i][j] = input + Math.min(dp[i-1][(j+1)%3], dp[i-1][(j+2)%3]);
 			}
 		}
 		
 		int answer = dp[N][0];
-		for(int i=0; i<3; i++) {
+		for(int i=1; i<3; i++) {
 			answer = Math.min(dp[N][i], answer);
 		}
 		
