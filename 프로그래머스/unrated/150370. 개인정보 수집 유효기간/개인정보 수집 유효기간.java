05b6a725a40aad91ae2e63e094bc3e79a3d25bfb
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-09-08 08:45:25 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 7.88 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> tempAnswer = new ArrayList<>();
+        Map<String, Integer> periods = new HashMap<>();
+        
+        LocalDate todayDate = LocalDate.parse(today.replace(".", "-"));
+        
+        for(String term : terms){
+            String[] t = term.split(" ");
+            periods.put(t[0], Integer.parseInt(t[1]));
+        }
+        
+        for(int p=0; p<privacies.length; p++){
+            String[] privacy = privacies[p].split(" ");
+            LocalDate agreeDate = LocalDate.parse(privacy[0].replace(".", "-"));
+            String termKind = privacy[1];
+            LocalDate limitDate = agreeDate.plusMonths(periods.get(termKind));
+
+            if(todayDate.isAfter(limitDate) || todayDate.equals(limitDate)){
+                tempAnswer.add(p+1);
+            }
+        }
+        
+        int[] answer = new int[tempAnswer.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = tempAnswer.get(i);
+        }
+        
+        return answer;
+    }
+}
