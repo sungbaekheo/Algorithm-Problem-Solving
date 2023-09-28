ff163488e4ae1f5c8fc3e8575bcd13ad4eeac0f1
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-09-28 19:03:08 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n>=a){
+            int newBottle = n/a*b;
+            answer+=newBottle;   
+            n = newBottle + n%a;
+        }
+        
+        return answer;
+    }
+}
