dce9e4deee6c5a30aaec215877ca5405d0bc4487
HeoSungbaek <112380381+sungbaekheo@users.noreply.github.com>
2023-10-03 21:41:11 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        
+        for(int i=0; i<date1.length; i++){
+            if(date1[i] < date2[i]){
+                return 1;
+            } else if(date1[i] == date2[i]){
+                continue;
+            } else{
+                return 0;
+            }
+        }
+        return 0;
+    }
+}
